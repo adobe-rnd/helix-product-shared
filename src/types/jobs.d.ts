211b3f5adc6cebffdac9f47e8e8d4cf9d528afea
Max Edell <maxed@adobe.com>
2025-12-09 12:46:23 -0800

    fix: update job typings

--- src/types/jobs.d.ts
+++ src/types/jobs.d.ts
@@ -1,16 +1,13 @@
 export interface IndexingJobProduct {
-  sku: string;
+  path: string;
   action?: 'add' | 'update' | 'delete' | string; // defaults to update
 }
 
 export interface IndexingJob {
   org: string;
   site: string;
-  storeCode: string;
-  storeViewCode: string;
   products: IndexingJobProduct[];
   timestamp: number;
 }
 
-export interface ImageCollectorJob extends IndexingJob {
-}
+export interface ImageCollectorJob extends IndexingJob { }
